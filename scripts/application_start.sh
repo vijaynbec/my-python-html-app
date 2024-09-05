21f0ea51da93730b5f2fce5f23c76e769a24800b
vijaynbec <119669904+vijaynbec@users.noreply.github.com>
2024-09-05 15:23:30 -0400

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,18 +1,18 @@
-# #!/bin/bash
-
-# echo "Running container..."
-# docker run --name flask_app -d -p 5000:5000 426924142575.dkr.ecr.us-east-1.amazonaws.com/flask_image:latest
 #!/bin/bash
 
-# Authenticate Docker to ECR
-echo "Authenticating Docker to ECR..."
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 426924142575.dkr.ecr.us-east-1.amazonaws.com
+echo "Running container..."
+docker run --name flask_app -d -p 5000:5000 426924142575.dkr.ecr.us-east-1.amazonaws.com/flask_image:latest
+# #!/bin/bash
 
-# Pull the Docker image
-echo "Pulling Docker image..."
-docker pull 426924142575.dkr.ecr.us-east-1.amazonaws.com/flask_image:latest
+# # Authenticate Docker to ECR
+# echo "Authenticating Docker to ECR..."
+# aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 426924142575.dkr.ecr.us-east-1.amazonaws.com
 
-# Run the Docker container
-echo "Running Docker container..."
-docker run -d -p 80:80 426924142575.dkr.ecr.us-east-1.amazonaws.com/flask_image:latest
+# # Pull the Docker image
+# echo "Pulling Docker image..."
+# docker pull 426924142575.dkr.ecr.us-east-1.amazonaws.com/flask_image:latest
+
+# # Run the Docker container
+# echo "Running Docker container..."
+# docker run -d -p 80:80 426924142575.dkr.ecr.us-east-1.amazonaws.com/flask_image:latest
 
