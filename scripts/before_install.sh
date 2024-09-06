f27ea30b243b94c65cfe53ea2f8555d9d270928d
vijaynbec <119669904+vijaynbec@users.noreply.github.com>
2024-09-06 11:42:10 -0400

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -12,20 +12,23 @@ if ! command -v docker &> /dev/null; then
     # Start Docker
     sudo systemctl start docker
     sudo systemctl enable docker
-	sudo systemctl status docker
-    sudo apt-get install awscli -y
+    sudo systemctl status docker
+    
 
     # Add the current user to Docker group
     sudo usermod -aG docker ubuntu
-	aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 426924142575.dkr.ecr.us-east-1.amazonaws.com
-	docker run --name flask_app -d -p 5000:5000 426924142575.dkr.ecr.us-east-1.amazonaws.com/flask_image:latest
     echo "Docker installed successfully."
 fi
     echo "Docker Found"
 
 if ! command -v aws &>/dev/null; then
     echo "AWS CLI not found. Installing AWS CLI..."
-    sudo apt-get update -y
-    sudo apt-get install awscli -y
+    # sudo apt-get update -y
+    # sudo apt-get install awscli -y
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 426924142575.dkr.ecr.us-east-1.amazonaws.com
+	
 fi
     echo "AWS CLI is already installed."
