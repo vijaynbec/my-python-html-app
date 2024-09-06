c932b3fef1a057c88d67d5e94e36de993d331336
vijaynbec <119669904+vijaynbec@users.noreply.github.com>
2024-09-06 12:06:24 -0400

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 echo "Running container..."
+docker ps -a
+docker stop flask_app || true
+docker rm flask_app || true
 docker run --name flask_app -d -p 5000:5000 426924142575.dkr.ecr.us-east-1.amazonaws.com/flask_image:latest
