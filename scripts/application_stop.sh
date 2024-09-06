7b14570a02a86e1050a535c38260191279b1676e
vijaynbec <119669904+vijaynbec@users.noreply.github.com>
2024-09-06 11:56:43 -0400

    Update application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -2,6 +2,10 @@
 
 echo "This Script is used to stop already running docker container, remove them and remove the image as well"
 
-sudo docker stop $(sudo docker ps -q)
-sudo docker rm $(sudo docker ps -a -q)
-sudo docker rmi $(sudo docker images -q)
+# sudo docker stop $(sudo docker ps -q)
+# sudo docker rm $(sudo docker ps -a -q)
+# sudo docker rmi $(sudo docker images -q)
+
+docker ps -a
+docker stop flask_app || true
+docker rm flask_app || true
